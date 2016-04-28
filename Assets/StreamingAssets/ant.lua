c0a2d353d22657f03be77701a87cdc9b7b83f892
Jack the Ripper <andreastheripper@aol.de>
2016-04-28 20:51:26 +0200

    ant.lua: ants move to apples and sugar

--- Assets/StreamingAssets/ant.lua
+++ Assets/StreamingAssets/ant.lua
@@ -1,12 +1,25 @@
 function init()
 	ant.update = update;
 	ant.sugarEnterView = sugarEnterView;
+	ant.appleEnterView = appleEnterView;
+	
+	ant.setDestination(20, 0);
+	hasTarget = false;
 end
 
 function update()
-
 end
 
 function sugarEnterView(sugar)
-	print("zucker!!!");
+	if not hasTarget then
+		ant.setDestinationGlobal(sugar.position.x, sugar.position.y);
+		hasTarget = true;
+	end
+end
+
+function appleEnterView(apple)
+	if not hasTarget then
+		ant.setDestinationGlobal(apple.position.x, apple.position.y);
+		hasTarget = true;
+	end
 end

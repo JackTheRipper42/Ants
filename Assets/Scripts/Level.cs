836cad6452e1dd77b6c7a4bb6c45bf0c97ad43eb
Jack the Ripper <andreastheripper@aol.de>
2016-05-04 16:50:00 +0200

    level prefab added

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Level : MonoBehaviour
+    {
+        public Transform Ground;
+
+        public Rect GetLevelBounds()
+        {
+            var scale = CalculateTotalScale(Ground);
+            return new Rect(
+                Ground.transform.position.x,
+                Ground.transform.position.z,
+                scale.x,
+                scale.z);
+        }
+
+        private static Vector3 CalculateTotalScale(Transform transform)
+        {
+            if (transform == null)
+            {
+                return new Vector3(1f, 1f, 1f);
+            }
+
+            var scale = transform.localScale;
+            var parentScale = CalculateTotalScale(transform.parent);
+            return new Vector3(scale.x*parentScale.x, scale.y*parentScale.y, scale.z*parentScale.z);
+        }
+    }
+}

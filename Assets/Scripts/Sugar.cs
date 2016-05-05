de2bcb7871d340cce083ea8e34f2b940f69590ab
Jack the Ripper <andreastheripper@aol.de>
2016-05-05 13:44:06 +0200

    the sugar pile shrinks when sugar is removed

--- Assets/Scripts/Sugar.cs
+++ Assets/Scripts/Sugar.cs
@@ -5,8 +5,30 @@ namespace Assets.Scripts
     public class Sugar : MonoBehaviour
     {
         public int InitialCapacity = 100;
+        private int _capacity;
 
-        public int Capacity { get; private set; }
+        public int Capacity
+        {
+            get { return _capacity; }
+            private set
+            {
+                if (value != _capacity)
+                {
+                    _capacity = value;
+                    OnCapacityChanged();
+                }
+            }
+        }
+
+        public bool PickSugar()
+        {
+            if (Capacity <= 0)
+            {
+                return false;
+            }
+            Capacity--;
+            return true;
+        }
 
         protected virtual void Start()
         {
@@ -21,14 +43,10 @@ namespace Assets.Scripts
             }
         }
 
-        public bool PickSugar()
+        private void OnCapacityChanged()
         {
-            if (Capacity <= 0)
-            {
-                return false;
-            }
-            Capacity = Capacity - 1;
-            return true;
+            var scale = Mathf.Pow((float) Capacity/InitialCapacity, 1/3f);
+            transform.localScale = new Vector3(scale, scale, scale);
         }
     }
 }
